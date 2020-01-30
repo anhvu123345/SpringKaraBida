58fd16d9a5b8bb44526bb0e6d62bbe716e6dec67
dangvuanh <dangvuanhcn@gmail.com>
2020-01-31 02:03:45 +0700

    init project

---
+++ src/main/java/org/anhvu0/SpringKaraBidaApplication.java
@@ -0,0 +1,13 @@
+package org.anhvu0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKaraBidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKaraBidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/anhvu0/SpringKaraBidaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anhvu0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKaraBidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
